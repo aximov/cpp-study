742fc18dfa9cdd4f3ce3fd85703eb489b68fa0b1
Akimoto Daichi <deltalphan+github@gmail.com>
2018-02-13 16:55:38 +0900

    範囲for文

---
+++ stroustrup-ref/range-for.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+void print() {
+  int v[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+  for (auto x : v) cout << x << endl;
+  for (auto x : {10, 21, 32, 43, 54, 65}) cout << x << endl;
+}
+
+int main() { print(); }
