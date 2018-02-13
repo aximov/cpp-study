b9da8b6cde30a93e18eee3d805c8a6cd7c629586
Akimoto Daichi <deltalphan+github@gmail.com>
2018-02-13 20:13:32 +0900

    length_error

--- stroustrup-ref/Vector.cc
+++ stroustrup-ref/Vector.cc
@@ -1,6 +1,15 @@
 #include "Vector.hh"
+#include <stdexcept>
+using namespace std;
 
 // define Vector class
-Vector::Vector(int s) : elem{new double[s]}, sz{s} {}
-double& Vector::operator[](int i) { return elem[i]; }
+Vector::Vector(int s) {
+  if (s < 0) throw length_error{"length error"};
+  elem = new double[s];
+  sz = s;
+}
+double& Vector::operator[](int i) {
+  if (i < 0 || size() <= i) throw out_of_range{"Vector::operator[]"};
+  return elem[i];
+}
 int Vector::size() { return sz; }
